94079e96dc9389d603da553066bcc9f8a1aeb851
iamshahzeb <syedshahzebakhtar@gmail.com>
2022-10-17 14:51:18 +0500

    fix: use native img tag
    re: GS-009

--- src/components/github-search/user-list/index.tsx
+++ src/components/github-search/user-list/index.tsx
@@ -1,21 +1,11 @@
-import Image from 'next/image';
-
 const UserList = ({ users }: { users: any }) => {
- const imgLoader = (src: any) => {
-  return src;
- };
  return (
   <div>
    {(users || []).map((user: any) => (
     <div className="result" key={user.id}>
      <span>{user.login}</span>
-     <Image
-      width={20}
-      height={20}
-      loader={() => imgLoader(user.avatar_url)}
-      src={user.avatar_url}
-      alt="avatar"
-     />
+     {/* eslint-disable-next-line @next/next/no-img-element */}
+     <img src={user.avatar_url} alt="avatar" />
      <a href={user.url} target="_blank" rel="noreferrer"></a>
     </div>
    ))}
